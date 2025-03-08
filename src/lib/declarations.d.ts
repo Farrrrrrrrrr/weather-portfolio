8f7f404d2b8c78d7c73baa98c740eca6023d9a99
Farrell <farrellsiwy@gmail.com>
2025-03-09 04:21:55 +0700

    fix first commit

--- src/lib/declarations.d.ts
+++ src/lib/declarations.d.ts
@@ -10,4 +10,55 @@ declare module 'openweather-apis' {
   // Add other methods as needed
 }
 
+declare module 'weather-js' {
+  interface WeatherOptions {
+    search: string;
+    degreeType: string;
+  }
+  
+  interface WeatherLocation {
+    name: string;
+    zipcode?: string;
+    lat: string;
+    long: string;
+    timezone: string;
+    alert?: string;
+    degreetype: string;
+    imagerelativeurl: string;
+  }
+  
+  interface WeatherForecast {
+    low: string;
+    high: string;
+    skycodeday: string;
+    skytextday: string;
+    date: string;
+    day: string;
+    shortday: string;
+    precip: string;
+  }
+  
+  interface WeatherCurrent {
+    temperature: string;
+    skycode: string;
+    skytext: string;
+    date: string;
+    observationtime: string;
+    observationpoint: string;
+    feelslike: string;
+    humidity: string;
+    winddisplay: string;
+    windspeed: string;
+    imageUrl: string;
+  }
+  
+  interface WeatherResult {
+    location: WeatherLocation;
+    current: WeatherCurrent;
+    forecast: WeatherForecast[];
+  }
+  
+  export function find(options: WeatherOptions, callback: (err: Error | null, result: WeatherResult[]) => void): void;
+}
+
 // If we have other modules that need type declarations, add them here
